01551d96ca5081a87601249ed63008f41fd72b2b
alexfedy <916falex@gmail.com>
2023-09-15 14:38:42 -0700

    handler methods completed

--- application/routes.go
+++ application/routes.go
@@ -4,22 +4,27 @@ import (
 	"net/http"
 
 	"github.com/alexfedy/Go-API/handler"
+	"github.com/alexfedy/Go-API/repository/order"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
-	router.Route("/orders", loadOrderRoutes)
-	return router
+	router.Route("/orders", a.loadOrderRoutes)
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
 	router.Get("/{id}", orderHandler.GetByID)
